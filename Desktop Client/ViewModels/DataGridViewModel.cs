6957bb6da6067fd58b1d47c7bee0911969b7a9f0
pappkristof@sulid.hu <pappkristof@sulid.hu>
2018-09-17 19:54:00 +0200

    Add project files.

---
+++ Desktop Client/ViewModels/DataGridViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+using Desktop_Client.Models;
+using Desktop_Client.Services;
+
+using GalaSoft.MvvmLight;
+
+namespace Desktop_Client.ViewModels
+{
+    public class DataGridViewModel : ViewModelBase
+    {
+        public ObservableCollection<SampleOrder> Source
+        {
+            get
+            {
+                // TODO WTS: Replace this with your actual data
+                return SampleDataService.GetGridSampleData();
+            }
+        }
+    }
+}
---
+++ Desktop Client/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using CommonServiceLocator;
+
+using Desktop_Client.Services;
+using Desktop_Client.Views;
+
+using GalaSoft.MvvmLight.Ioc;
+
+namespace Desktop_Client.ViewModels
+{
+    [Windows.UI.Xaml.Data.Bindable]
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register(() => new NavigationServiceEx());
+            SimpleIoc.Default.Register<ShellViewModel>();
+            Register<DataGridViewModel, DataGridPage>();
+        }
+
+        public DataGridViewModel DataGridViewModel => ServiceLocator.Current.GetInstance<DataGridViewModel>();
+
+        public ShellViewModel ShellViewModel => ServiceLocator.Current.GetInstance<ShellViewModel>();
+
+        public NavigationServiceEx NavigationService => ServiceLocator.Current.GetInstance<NavigationServiceEx>();
+
+        public void Register<VM, V>()
+            where VM : class
+        {
+            SimpleIoc.Default.Register<VM>();
+
+            NavigationService.Configure(typeof(VM).FullName, typeof(V));
+        }
+    }
+}
