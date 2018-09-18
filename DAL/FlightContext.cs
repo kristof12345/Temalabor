a899266f31432522f517cf31fdfbf601f36d3b1f
Gábor Patyi <patyi.gabor25@gmail.com>
2018-09-18 20:14:27 +0200

    commit

---
+++ DAL/FlightContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    public class FlightContext: DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Seat> Seats { get; set; }
+        //public DbSet<ReserveSeat> ReserveSeats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS;Database=FlightDB;Trusted_Connection=True;");
+        }
+    }
+}
