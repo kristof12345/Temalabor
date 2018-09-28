19e7fe98e40af66e7395fd752051fe67ae589fcb
pappkristof1 <pappkristof1@gmail.com>
2018-09-28 10:16:38 +0200

    a

--- DTO/ListFlights_DTO.cs
+++ DTO/ListFlights_DTO.cs
@@ -5,7 +5,7 @@ namespace DTO
     public class ListFlights_DTO
     {
         //A listázandó repülők paraméterei
-        //Pl.: Csak a szabad hellyel rendelkezők
+        //Csak a szabad hellyel rendelkezők
         public bool OnlyAvailable { get; set; }
         //Csak az adott dátumon (az idópont nem számít)
         public DateTime AtDate { get; set; }
