0382f242f088d66fb0ecd3f6bb2d3dde3eb197b4
Gábor Patyi <patyi.gabor25@gmail.com>
2018-09-28 10:24:22 +0200

    commit

---
+++ TempProba/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DAL;
+
+namespace TempProba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new FlightContext())
+            {
+
+                var std = new Flight();
+                std.departure = "Los Angeles";
+
+                context.Flights.Add(std);
+                context.SaveChanges();
+            }
+        }
+    }
+}
