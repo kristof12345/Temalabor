0a302a1e44c2cab2444be7ebfc73077d47f0df61
Gábor Patyi <patyi.gabor25@gmail.com>
2018-09-29 13:56:42 +0200

    commit

---
+++ DAL/Migrations/20180929100230_CreateFlightDB.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class CreateFlightDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    flightID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    date = table.Column<DateTime>(nullable: false),
+                    departure = table.Column<string>(nullable: true),
+                    destination = table.Column<string>(nullable: true),
+                    freeSeats = table.Column<int>(nullable: false),
+                    planeType = table.Column<string>(nullable: true),
+                    status = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.flightID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Seats",
+                columns: table => new
+                {
+                    seatID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    flightID = table.Column<int>(nullable: true),
+                    IsReserved = table.Column<bool>(nullable: false),
+                    seatType = table.Column<string>(nullable: true),
+                    price = table.Column<int>(nullable: false),
+                    Xcord = table.Column<int>(nullable: false),
+                    Ycord = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Seats", x => x.seatID);
+                    table.ForeignKey(
+                        name: "FK_Seats_Flights_flightID",
+                        column: x => x.flightID,
+                        principalTable: "Flights",
+                        principalColumn: "flightID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Seats_flightID",
+                table: "Seats",
+                column: "flightID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Seats");
+
+            migrationBuilder.DropTable(
+                name: "Flights");
+        }
+    }
+}
--- Desktop/Services/HttpService.cs
+++ Desktop/Services/HttpService.cs
@@ -8,12 +8,25 @@ namespace Desktop.Services
     public class HttpService
     {
         private static HttpClient client = new HttpClient();
-        private static string uri = "https://www.google.hu/";
-        //private static string uri = "https://localhost:5001/";
+        private static HttpClientHandler handler = new HttpClientHandler();
+        //private static string uri = "https://www.google.hu/";
+        private static string uri = "https://localhost:44376/api/reserve";
 
         //Repülő hozzáadása
         public static async Task PostAddFlightAsync(Flight_DTO addRequest)
         {
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+
+            handler.ServerCertificateCustomValidationCallback =
+
+                (httpRequestMessage, cert, cetChain, policyErrors) =>
+
+                {
+
+                    return true;
+
+                };
+            client = new HttpClient(handler);
             HttpResponseMessage response = await client.PostAsJsonAsync(uri, addRequest);
             var contents = await response.Content.ReadAsStringAsync();
             Debug.WriteLine(contents);
