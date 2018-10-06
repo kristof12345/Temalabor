a9606b9bb41f9fe68af2d69581b161cba276fa2a
pappkristof1 <pappkristof1@gmail.com>
2018-10-06 18:29:15 +0200

    plane designer page

--- Desktop/ViewModels/DesignerViewModel.cs
+++ Desktop/ViewModels/DesignerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using GalaSoft.MvvmLight;
+﻿using DTO;
+using GalaSoft.MvvmLight;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,18 @@ namespace Desktop.ViewModels
 {
     public class DesignerViewModel : ViewModelBase
     {
+        private List<Seat> seats = new List<Seat>();
+
+        public String NumberOfSeats
+        {
+            get{ return seats.Count.ToString(); }
+            set { }
+        }
+        public void addSeat(double x, double y, int seatType=1)
+        {
+            Seat s = new Seat(seats.Count);
+            seats.Add(s);
+            RaisePropertyChanged("NumberOfSeats");
+        }
     }
 }
--- Desktop/Views/DesignerPage.xaml.cs
+++ Desktop/Views/DesignerPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using Desktop.ViewModels;
+﻿using Desktop.Services;
+using Desktop.UserControls;
+using Desktop.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -14,23 +17,53 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace Desktop.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class DesignerPage : Page
     {
         public DesignerPage()
         {
             this.InitializeComponent();
+            canvas.PointerPressed += clicked;
+        }
+
+        private void clicked(object sender, PointerRoutedEventArgs e)
+        {
+            Debug.WriteLine("Click");
+            var pos = e.GetCurrentPoint(canvas);
+            tbCordX.Text = pos.Position.X.ToString();
+            tbCordY.Text = pos.Position.Y.ToString();
+            SeatUserControl newSeat = new SeatUserControl(1, false);
+            //Left=0, Top=X, Right=Y, Bottom=0
+            newSeat.Margin = new Thickness(pos.Position.X-10, pos.Position.Y-15, 0, 0);
+            canvas.Children.Add(newSeat);
+            tbNum.Text = ViewModel.NumberOfSeats;
         }
 
         private DesignerViewModel ViewModel
         {
             get { return DataContext as DesignerViewModel; }
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            if(SignInService.User == null)
+            {
+                DisplayNoUserDialog();
+            }
+        }
+
+        //Dialógusablak
+        private async void DisplayNoUserDialog()
+        {
+            ContentDialog noUser = new ContentDialog
+            {
+                Title = "You're not signed in.",
+                Content = "Plese sign in to continue.",
+                CloseButtonText = "Ok"
+            };
+            ContentDialogResult result = await noUser.ShowAsync();
+            this.Frame.Navigate(typeof(UserPage));
         }
     }
 }
