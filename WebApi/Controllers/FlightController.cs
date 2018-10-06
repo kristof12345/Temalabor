8dd4010aa67ac6f68a5f429fd7b1ecf5c8982ec4
pappkristof1 <pappkristof1@gmail.com>
2018-10-06 13:34:59 +0200

    commit

--- WebApi/Controllers/FlightController.cs
+++ WebApi/Controllers/FlightController.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DTO;
 using WebApi.Controllers;
-using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
