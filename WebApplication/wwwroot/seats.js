72d2f9b92b085ec24a2a3159dd5fc659fdac1d8a
Gergely Baranyai <bargergo@hotmail.com>
2018-11-04 15:59:25 +0100

    Refaktor

--- WebApplication/wwwroot/seats.js
+++ WebApplication/wwwroot/seats.js
@@ -1,19 +1,34 @@
-$(function(){
-  $.ajax({url:'mock/flight.json',success:function(d){
-    //var t = $("#flightsTable");
-      for (var i = 0; i < d.length; ++i) {
-          var element = d[i];
-          var tr = $("<tr/>")
-              .append($('<td/>', { text: element.planeTypeName }))
-              .append($('<td/>', { text: element.departure }))
-              .append($('<td/>', { text: element.destination }))
-              .append($('<td/>', { text: element.date }))
-              .append($('<td/>', { text: element.status }))
-              .append($('<td/>', { text: element.freeSeats }))
-              .append($('<td/>', { text: element.numberOfSeats }))
-              .append('<td><button type="button">Book</button> </td>');
-          $('#flightsTable > tbody:last-child').append(tr);
-    }
+/*$(function(){
+    
+});*/
+
+$(document).ready(function () {
+    getFlights();
+});
+
+function getFlights() {
+    $.ajax({
+        //url: 'mock/flight.json',
+        type: 'GET',
+        url: 'https://localhost:5001/api/flight',
+        success: function (d) {
+            //var t = $("#flightsTable");
+            for (var i = 0; i < d.length; ++i) {
+                var element = d[i];
+                var tr = $("<tr/>")
+                    .append($('<td/>', { text: element.planeTypeName }))
+                    .append($('<td/>', { text: element.departure }))
+                    .append($('<td/>', { text: element.destination }))
+                    .append($('<td/>', { text: element.date }))
+                    .append($('<td/>', { text: element.status }))
+                    .append($('<td/>', { text: element.freeSeats }))
+                    .append($('<td/>', { text: element.numberOfSeats }))
+                    .append('<td><button type="button">Book</button> </td>');
+                $('#flightsTable > tbody:last-child').append(tr);
+            }
+        }
+    })
+}
 /*var r = Raphael(0, 0, 300, 150);
 
 r.rect(0, 0, 50, 50)
@@ -28,6 +43,3 @@ r.rect(75, 0, 50, 50)
         alert('second rectangle clicked');
      });
    }*/
- }
-})
-});
