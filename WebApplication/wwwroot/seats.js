cbe143f028b7b1b217de4592e05ded16994aaa6e
Gergely Baranyai <bargergo@hotmail.com>
2018-11-04 20:34:37 +0100

    Székek kirajzolása

--- WebApplication/wwwroot/seats.js
+++ WebApplication/wwwroot/seats.js
@@ -2,9 +2,41 @@
     
 });*/
 
+var r = undefined;
+
 $(document).ready(function () {
     getFlights();
 });
+
+function getSeats(flightId) {
+    if (r == undefined) {
+        r = Raphael(0, 100, 1000, 700);
+    } else {
+        r.clear();
+    }
+    r.image("img/Antonov125.png", 0, 0, 1000, 700);
+    $.ajax({
+        type: 'GET',
+        url: 'https://localhost:5001/api/seat/flightID/' + flightId,
+        success: function (d) {
+            
+            for (var i = 0; i < d.length; ++i) {
+                console.log(d[i]);
+                var color = "green";
+
+                if (d[i].reserved)
+                    color = "red";
+
+                r.rect(d[i].coordinates.x, d[i].coordinates.y, 30, 30)
+                    .attr({ fill: color })
+                    .click(function () {
+                        alert('rectangle clicked');
+                    });
+            }
+        }
+    });
+
+}
 
 function getFlights() {
     $.ajax({
@@ -23,7 +55,7 @@ function getFlights() {
                     .append($('<td/>', { text: element.status }))
                     .append($('<td/>', { text: element.freeSeats }))
                     .append($('<td/>', { text: element.numberOfSeats }))
-                    .append('<td><button type="button">Book</button> </td>');
+                    .append('<td><input id=' + element.flightId + ' type="button" value="Book" onclick="getSeats(this.id)"> </td>');
                 $('#flightsTable > tbody:last-child').append(tr);
             }
         }
