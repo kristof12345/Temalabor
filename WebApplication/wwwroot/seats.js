8a322a602ea5cabd6ca610640e9c14c02b5646dc
Gergely Baranyai <bargergo@hotmail.com>
2018-12-13 15:12:37 +0100

    Jegyárak kijelzése

--- WebApplication/wwwroot/seats.js
+++ WebApplication/wwwroot/seats.js
@@ -28,7 +28,7 @@ function createFlightsTable() {
     return table;
 }
 
-function getSeats(flightId) {
+function getSeats(flight) {
     if (r == undefined) {
         $('#placeForCanvas').css('border','1px solid #aaa');
         r = Raphael('placeForCanvas', '100%', '100%');
@@ -40,7 +40,7 @@ function getSeats(flightId) {
     r.image("img/Antonov125.png", 0, 0, 1000, 700);
     $.ajax({
         type: 'GET',
-        url: API_BASE_URL + 'seat/flightID/' + flightId,
+        url: API_BASE_URL + 'seat/flightID/' + flight.flightId,
         success: function (d) {
             
             for (var i = 0; i < d.length; ++i) {
@@ -52,12 +52,25 @@ function getSeats(flightId) {
                 var seat = r.rect(d[i].coordinates.x-150, d[i].coordinates.y, 30, 30)
                 seat.attr({ fill: color })
                 seat.data("seatId", d[i].seatId);
-                seat.data("flightId", flightId);
+                seat.data("flightId", flight.flightId);
                 seat.data("reserved", d[i].reserved);
+                if (d[i].seatType === 0) {
+                    seat.data("price", flight.normalPrice);
+                    seat.data("category", "normal");
+                } else {
+                    seat.data("price", flight.firstClassPrice);
+                    seat.data("category", "first class");
+                }
+
                 seat.click(function () {
                     if (!this.data("reserved")) {
                         //console.log("flightId: " + this.data("flightId") + "seatId: " + this.data("seatId"));
-                        var clickedSeat = { flightId: this.data("flightId"), seatId: this.data("seatId") };
+                        var clickedSeat = {
+                            flightId: this.data("flightId"),
+                            seatId: this.data("seatId"),
+                            price: this.data("price"),
+                            category: this.data("category")
+                        };
                         var inSelectedSeats = selectedSeats.filter(row => row.flightId === clickedSeat.flightId && row.seatId === clickedSeat.seatId);
                         if (inSelectedSeats.length > 0) {
                             var index = selectedSeats.indexOf(inSelectedSeats[0]);
@@ -70,9 +83,14 @@ function getSeats(flightId) {
                         var listOfSelectedSeats = $('<ul class="list-group">');
                         selectedSeats.forEach(ss => {
                             let listElement = $('<il class="list-group-item">');
-                            listElement.append("JáratAz: " + ss.flightId + " HelyAz: " + ss.seatId);
+                            listElement.append("JáratAz: " + ss.flightId + " HelyAz: " + ss.seatId + " " + ss.category + " " + ss.price + " Ft");
                             listOfSelectedSeats.append(listElement);
                         });
+                        let listElement = $('<il class="list-group-item">');
+                        let sum = 0;
+                        selectedSeats.forEach(ss => sum += ss.price);
+                        listElement.append("Összesen: " + sum + " Ft");
+                        listOfSelectedSeats.append(listElement);
                         $('#selectedSeats').replaceWith('<div id="selectedSeats" class="col">Kiválasztott helyek:</div>');
                         $('#selectedSeats').append(listOfSelectedSeats);
                         //console.log(selectedSeats);
@@ -117,7 +135,7 @@ function loadFlightsTable(table, flight) {
                 .append($('<td/>', { text: element.numberOfSeats }));
                 //.append('<td><input id=' + element.flightId + ' type="button" value="Book" onclick="getSeats(this.id)"> </td>');
             tr.click(function () {
-                getSeats(element.flightId);
+                getSeats(element);
             });
             //$('#flightsTable > tbody:last-child')
             table.append(tr);
